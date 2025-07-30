93e880b440acfd937ffdcedca0643f02427316f4
Gabriel-GDS-dev <gabriel.gsouzza6@gmail.com>
2025-07-30 14:25:07 -0300

    Teste

--- colina-verde-admin/backend/server.js
+++ colina-verde-admin/backend/server.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+const path = require("path");
 const app = express();
-const session = require('express-session');
-app.use(session({
-  secret: 'colina-verde-secret',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: null
-  }
-}));
+const session = require("express-session");
+app.use(
+  session({
+    secret: "colina-verde-secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: null,
+    },
+  })
+);
 app.use(cors());
 app.use(express.json());
-app.use('/api', routes);
+app.use("/api", routes);
 // Servir arquivos estáticos da pasta uploads
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
+);
